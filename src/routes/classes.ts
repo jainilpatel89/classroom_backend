6057a6df51106df91c5ceb6aa27557ff67aba443
Jainil Patel <jainilpatel89@gmail.com>
2026-04-10 18:42:21 -0400

    implement class details

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -97,4 +97,32 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const classId = Number(req.params.id);
+
+    if(!Number.isFinite(classId)) return res.status(400).json({ error: "No Class found." });
+
+    const [classDetails] = await db.select({
+        ...getTableColumns(classes),
+        subject: {
+            ...getTableColumns(subjects),
+        },
+        department: {
+            ...getTableColumns(departments),
+        },
+        teacher: {
+            ...getTableColumns(user),
+        },
+    })
+        .from(classes)
+        .leftJoin(subjects, eq(classes.subjectId, subjects.id))
+        .leftJoin(user, eq(classes.teacherId, user.id))
+        .leftJoin(departments, eq(subjects.departmentId, departments.id))
+        .where(eq(classes.id, classId));
+
+    if(!classDetails) return res.status(404).json({ error: "No Class found." });
+
+    res.status(200).json({ data: classDetails });
+})
+
 export default router;
