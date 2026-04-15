ad320a50cd142d8965285246679013de451a8f25
Jainil Patel <jainilpatel89@gmail.com>
2026-04-15 16:40:25 -0400

    implement dashboard stats

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import subjectsRouter from './routes/subjects.js';
 import userRoutes from './routes/users.js';
 import classesRouter from './routes/classes.js';
 import departmentsRouter from './routes/departments.js';
+import statsRouter from './routes/stat.js';
 import enrollmentsRouter from './routes/enrollments.js';
 import securityMiddleware from './middleware/security.js';
 import { toNodeHandler } from 'better-auth/node';
@@ -32,6 +33,7 @@ app.use('/api/subjects', subjectsRouter);
 app.use('/api/users', userRoutes);
 app.use('/api/classes', classesRouter);
 app.use('/api/departments', departmentsRouter);
+app.use('/api/stats', statsRouter);
 app.use('/api/enrollments', enrollmentsRouter);
 
 app.get('/', (req, res) => {
